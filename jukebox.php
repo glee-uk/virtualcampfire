f73eff99c127150c80b97b55d3135eb5c74e120a
Tim Pizey <timp@paneris.org>
2014-07-15 11:37:03 +0100

    Add fork me ribbon

--- jukebox.php
+++ jukebox.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-
 <link href="song_styles.css" rel="stylesheet" type="text/css" />
-
 
 <title>Virtual Campfire</title>
 <script src="sorttable.js"></script>
@@ -29,6 +25,9 @@ function choose(url) {
 
 </head>
 <body>
+
+<a href="https://github.com/timp/virtualcampfire"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/652c5b9acfaddf3a9c326fa6bde407b87f7be0f4/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f6f72616e67655f6666373630302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_orange_ff7600.png"></a>
+
 <audio 
   id="player" 
   type="audio/mpeg"
